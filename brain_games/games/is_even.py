2b073e97df71204523e10c50fb0b010c73500383
lime <green4lime@gmail.com>
2024-12-17 12:14:18 +0300

    Changes to be committed: 	deleted:    .github/workflows/README.md 	deleted:    .github/workflows/hexlet-check.yml 	deleted:    Makefile 	deleted:    README.md 	deleted:    brain_games/__init__.py 	deleted:    brain_games/cli.py 	deleted:    brain_games/games/__init__.py 	deleted:    brain_games/games/calc.py 	deleted:    brain_games/games/gcd.py 	deleted:    brain_games/games/is_even.py 	deleted:    brain_games/games/prime.py 	deleted:    brain_games/games/progression.py 	deleted:    brain_games/run_game.py 	deleted:    brain_games/scripts/__init__.py 	deleted:    brain_games/scripts/brain_calc.py 	deleted:    brain_games/scripts/brain_even.py 	deleted:    brain_games/scripts/brain_games.py 	deleted:    brain_games/scripts/brain_gcd.py 	deleted:    brain_games/scripts/brain_prime.py 	deleted:    brain_games/scripts/brain_progression.py 	deleted:    brain_games/welcome.py 	deleted:    poetry.lock 	deleted:    pyproject.toml 	deleted:    setup.cfg

--- brain_games/games/is_even.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def game_is_even():
-    instruction = 'Answer "yes" if the number is even, otherwise answer "no".'
-    num = random.randrange(50)
-    is_even = "yes" if num % 2 == 0 else "no"
-    return (num, is_even, instruction)
